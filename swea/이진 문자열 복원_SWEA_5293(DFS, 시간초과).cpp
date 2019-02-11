278e51f672544e579fa0fe6807e6c1837f23521e
DevTaeho <42051165+DevTaeho@users.noreply.github.com>
2019-02-11 20:58:18 +0900

    Create 이진 문자열 복원_SWEA_5293(DFS, 시간초과).cpp

---
+++ swea/이진 문자열 복원_SWEA_5293(DFS, 시간초과).cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string result;
+
+void solve(string res, int a, int b, int c, int d);
+
+int main() {
+	int testcase;
+	cin >> testcase;
+
+	for (int tc = 1; tc <= testcase; tc++) {
+		int A, B, C, D;
+		cin >> A >> B >> C >> D;
+
+		result = "\0";
+		solve("0", A, B, C, D);
+		solve("1", A, B, C, D);
+		if (result == "\0") result = "impossible";
+
+		cout << "#" << tc << ' ' << result << '\n';
+	}
+
+	return 0;
+}
+
+void solve(string res, int a, int b, int c, int d) {
+	if (result != "\0") return;
+	if (b - c > 1 || c - b > 1) return;
+	if (a < 0 || b < 0 || c < 0 || d < 0) return;
+	if (a == 0 && b == 0 && c == 0 && d == 0) {
+		result = res;
+		return;
+	}
+	
+	if (res.back() == '0') {
+		solve(res + "1", a, b - 1, c, d);
+		solve(res + "0", a - 1, b, c, d);
+	}
+	else if (res.back() == '1') {
+		solve(res + "1", a, b, c, d - 1);
+		solve(res + "0", a, b, c - 1, d);
+	}
+}
