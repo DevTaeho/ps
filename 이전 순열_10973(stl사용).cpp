aa8329520ccef64c402b7fef763c0701503f5dc7
DevTaeho <42051165+DevTaeho@users.noreply.github.com>
2019-02-22 19:46:43 +0900

    Add files via upload

---
+++ 이전 순열_10973(stl사용).cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+vector<int> a;
+
+int main() {
+	int n; scanf("%d", &n); a.resize(n);
+	for (int i = 0; i < n; i++) scanf("%d", &a[i]);
+	if (prev_permutation(a.begin(), a.end())) {
+		for (int i = 0; i < n; i++) printf("%d ", a[i]);
+	}
+	else printf("-1");
+	return 0;
+}
